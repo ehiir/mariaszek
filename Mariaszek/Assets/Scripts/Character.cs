a302307746bc3cf2e5795629bf30a93e86f976b0
Frankiplier <agatarucka@gmail.com>
2025-01-21 12:12:34 +0100

    Created scriptable object script

---
+++ Mariaszek/Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+
+public class Character : ScriptableObject
+{
+    public string name;
+    public Sprite portrait1;
+    public Sprite portrait2;
+}
