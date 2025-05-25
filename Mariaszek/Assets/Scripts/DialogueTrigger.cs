001e0a4b2db490bec45e2047e9c8423b186fd60f
ehiir <162104704+ehiir@users.noreply.github.com>
2025-05-25 19:50:56 +0200

    interakcje w srodku mariaszka dzialaja (nie ma spritow doe)

--- Mariaszek/Assets/Scripts/DialogueTrigger.cs
+++ Mariaszek/Assets/Scripts/DialogueTrigger.cs
@@ -9,6 +9,12 @@ public class DialogueTrigger : MonoBehaviour
 
     public bool oldPerson = false;
     public bool dog = false;
+    public bool mareczek = false;
+    public bool detektyw = false;
+    public bool tarociara = false;
+    public bool ardenia = false;
+    public bool hotelowner2 = false;
+    public bool kanapka = false;
 
     void Update()
     {
@@ -21,6 +27,36 @@ public class DialogueTrigger : MonoBehaviour
         {
             Debug.Log("Gadasz z psem pojebie");
             // dialogueRunner.StartDialogue("Dog");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && mareczek == true)
+        {
+           dialogueRunner.StartDialogue("Mareczek");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && detektyw == true)
+        {
+           dialogueRunner.StartDialogue("Detektyw");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && tarociara == true)
+        {
+           dialogueRunner.StartDialogue("staratarociara");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && ardenia == true)
+        {
+           dialogueRunner.StartDialogue("Ardenia");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && hotelowner2 == true)
+        {
+           dialogueRunner.StartDialogue("hotelowner_int");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && kanapka == true)
+        {
+           dialogueRunner.StartDialogue("kanapka");
         }
     }
 
@@ -35,6 +71,36 @@ public class DialogueTrigger : MonoBehaviour
         {
             dog = true;
         }
+
+        if (other.tag == "Mareczek")
+        {
+            mareczek = true;
+        }
+
+        if (other.tag == "Detektyw")
+        {
+            detektyw = true;
+        }
+
+         if (other.tag == "Tarociara")
+        {
+            tarociara = true;
+        }
+
+         if (other.tag == "Ardenia")
+        {
+            ardenia = true;
+        }
+
+         if (other.tag == "Hotelowner2")
+        {
+            hotelowner2 = true;
+        }
+        
+         if (other.tag == "Kanapka")
+        {
+            kanapka = true;
+        }
     }
 
     void OnTriggerExit (Collider other)
@@ -47,6 +113,31 @@ public class DialogueTrigger : MonoBehaviour
         if (other.tag == "Dog")
         {
             dog = false;
+        }
+
+          if (other.tag == "Mareczek")
+        {
+            mareczek = false;
+        }
+
+        if (other.tag == "Detektyw")
+        {
+            detektyw = false;
+        }
+
+          if (other.tag == "Ardenia")
+        {
+            ardenia = false;
+        }
+
+          if (other.tag == "Hotelowner2")
+        {
+            hotelowner2 = false;
+        }
+
+         if (other.tag == "Kanapka")
+        {
+            kanapka = false;
         }
     }
 }
