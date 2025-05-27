2597654ab0a8238f4833ce8d4a499ed31deee16d
Frankiplier <agatarucka@gmail.com>
2025-05-27 18:54:53 +0200

    NPCS animations

---
+++ Mariaszek/Assets/Scripts/StoryCharacters.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class StoryCharacters : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public Sprite[] spriteArray;
+    Animator dialogueAnimation;
+
+    void Start()
+    {
+        dialogueAnimation = gameObject.GetComponent<Animator>();
+    }
+
+    [YarnCommand("old1")]
+    public void ChangeSprite9()
+    {
+        sr.sprite = spriteArray[0];
+        dialogueAnimation.SetTrigger("Old1");
+        Debug.Log("Stary1 na ekranie");
+    }
+
+    [YarnCommand("old2")]
+    public void ChangeSprite10()
+    {
+        sr.sprite = spriteArray[1];
+        dialogueAnimation.SetTrigger("Old2");
+        Debug.Log("Stary2 na ekranie");
+    }
+
+    [YarnCommand("mareczek1")]
+    public void ChangeSprite11()
+    {
+        sr.sprite = spriteArray[2];
+        dialogueAnimation.SetTrigger("Mareczek1"); 
+        Debug.Log("Mareczek1 na ekranie");
+    }
+
+    [YarnCommand("mareczek2")]
+    public void ChangeSprite12()
+    {
+        sr.sprite = spriteArray[3];
+        dialogueAnimation.SetTrigger("Mareczek2"); 
+        Debug.Log("Mareczek2 na ekranie");
+    }
+
+    [YarnCommand("detektyw2")]
+    public void ChangeSprite13()
+    {
+        sr.sprite = spriteArray[4];
+        dialogueAnimation.SetTrigger("Detektyw1"); 
+        Debug.Log("Detektyw1 na ekranie");
+    }
+
+    [YarnCommand("detektyw2")]
+    public void ChangeSprite14()
+    {
+        sr.sprite = spriteArray[5];
+        dialogueAnimation.SetTrigger("Detektyw2"); 
+        Debug.Log("Detektyw2 na ekranie");
+    }
+
+    [YarnCommand("tarociara1")]
+    public void ChangeSprite15()
+    {
+        sr.sprite = spriteArray[6];
+        dialogueAnimation.SetTrigger("Tarociara1"); 
+        Debug.Log("Tarociara1 na ekranie");
+    }
+
+    [YarnCommand("tarociara2")]
+    public void ChangeSprite16()
+    {
+        sr.sprite = spriteArray[7];
+        dialogueAnimation.SetTrigger("Tarociara2"); 
+        Debug.Log("Tarociara2 na ekranie");
+    }
+
+    [YarnCommand("ardenia1")]
+    public void ChangeSprite17()
+    {
+        sr.sprite = spriteArray[8];
+        dialogueAnimation.SetTrigger("Ardenia1"); 
+        Debug.Log("Ardenia1 na ekranie");
+    }
+
+    [YarnCommand("ardenia2")]
+    public void ChangeSprite18()
+    {
+        sr.sprite = spriteArray[9];
+        dialogueAnimation.SetTrigger("Ardenia2"); 
+        Debug.Log("Ardenia2 na ekranie");
+    }
+
+    [YarnCommand("NPC_blank")]
+    public void ChangeSprite19()
+    {
+        sr.sprite = spriteArray[10];
+        dialogueAnimation.SetTrigger("Blank"); 
+        Debug.Log("Nic na ekranie");
+    }
+}
